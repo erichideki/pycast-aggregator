828009d2fbe3b2ea72090b6370c93ecf6dbcd066
erichideki <erichideki.is@gmail.com>
2021-11-08 18:39:48 -0300

    Add tests, create model and migration

--- content_aggregator/podcast/admin.py
+++ content_aggregator/podcast/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Episode
+
+@admin.register(Episode)
+class EpisodeAdmin(admin.ModelAdmin):
+    list_diplay = ("podcast_name", "title", "pub_date")
